779f47f0ad7daeebaffe828e5a39a86f9820d98c
Irfan <irfancanm@gmail.com>
2016-11-07 16:44:51 -0500

    My Django app, first commit

---
+++ Scripts/easy_install-3.5-script.py
@@ -0,0 +1,10 @@
+#!C:\Users\mohamik\AppData\Local\Programs\Python\Python35-32\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==20.10.1','console_scripts','easy_install-3.5'
+__requires__ = 'setuptools==20.10.1'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('setuptools==20.10.1', 'console_scripts', 'easy_install-3.5')()
+    )
